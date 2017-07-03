fedb518b939c550c2788ec688b8f4b8ccb206402
Adriano Carlos Verona <adriano.verona@gmail.com>
2017-07-03 11:06:00 -0300

    [scripting] More descriptive command line argument messages

--- UnityScript2CSharp/CommandLineArguments.cs
+++ UnityScript2CSharp/CommandLineArguments.cs
@@ -5,27 +5,32 @@ namespace UnityScript2CSharp
 {
     public class CommandLineArguments
     {
-        [Option('p', "projectPath", Required = true, HelpText = "Path of project to be converted.")] public string ProjectPath { get; set; }
+        [Option('u', "unityPath", Required = true, HelpText = "Unity installation path.")]
+        public string UnityPath { get; set; }
+
+        [Option('p', "projectPath", Required = true, HelpText = "Path of project to be converted.")]
+        public string ProjectPath { get; set; }
 
         [Option('r', "references", Min = 0, Max = 100, HelpText = "Assembly references required by the scripts (space separated list).")]
         public IEnumerable<string> References { get; set; }
 
-
-        [Option('d', "defines", HelpText = "A (comma separated) list of custom symbols to be defined")]
-        public string DefinesStr
+        [Option('s', "symbols", HelpText = "A (comma separated) list of custom symbols to be defined.")]
+        public string SymbolsStr
         {
-            get { return string.Join(",", _defines); }
-            set { _defines = new List<string>(value.Split(',')); }
+            get { return string.Join(",", _symbols); }
+            set { _symbols = new List<string>(value.Split(',')); }
         }
 
         [Option('o', "deleteOriginals",  HelpText = "Deletes original files (default is to rename).")] public bool RemoveOriginalFiles { get; set; }
 
-        [Option('s', HelpText = "Prints out the list of scripts being processed.")] public bool ShowScripts { get; set; }
+        [Option('d', HelpText = "Dumps out the list of scripts being processed.")] public bool DumpScripts { get; set; }
 
-        [Option('i',  HelpText = "Ignore errors.")] public bool IgnoreErrors { get; set; }
+        [Option('i',  HelpText = "Ignore compilation errors.")] public bool IgnoreErrors { get; set; }
 
-        public IEnumerable<string> Defines {  get { return _defines;  } }
+        [Option('v', "verbose", HelpText = "Show verbose messages.")] public bool Verbose { get; set; }
 
-        private IList<string> _defines = new List<string>();
+        public IEnumerable<string> Symbols {  get { return _symbols;  } }
+
+        private IList<string> _symbols = new List<string>();
     }
 }
--- UnityScript2CSharp/Program.cs
+++ UnityScript2CSharp/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using CommandLine;
+using CommandLine.Text;
 
 namespace UnityScript2CSharp
 {
@@ -12,43 +13,113 @@ namespace UnityScript2CSharp
         static int Main(string[] args)
         {
             var options  = Parser.Default.ParseArguments<CommandLineArguments>(args);
-            if (options.Errors.Any())
+            if (!IsValid(options))
             {
                 return -2;
             }
 
-            var editorSubFolder = String.Format("{0}Editor{0}", Path.DirectorySeparatorChar);
-            var pluginSubFolder = String.Format("{0}Plugins{0}", Path.DirectorySeparatorChar);
-
-            Console.WriteLine($"Editor: {editorSubFolder}\r\nPlugin: {pluginSubFolder}");
-
-            var allFiles = Directory.GetFiles(Path.Combine(options.Value.ProjectPath, "Assets"), "*.js", SearchOption.AllDirectories);
-            var filter = new Regex(string.Format(@"{0}{1}{0}|{0}{2}{0}", Path.DirectorySeparatorChar, editorSubFolder, pluginSubFolder), RegexOptions.Compiled);
-
-            var runtimeScripts = allFiles.Where(scriptPath => !filter.IsMatch(scriptPath)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath)});
-            var editorScripts = allFiles.Where(scriptPath => scriptPath.Contains(editorSubFolder)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath) });
-            var pluginScripts = allFiles.Where(scriptPath => scriptPath.Contains(pluginSubFolder)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath) });
-
-            if (options.Value.ShowScripts)
+            try
             {
-                DumpScripts("Runtime", runtimeScripts);
-                DumpScripts("Editor", editorScripts);
-                DumpScripts("Plugin", pluginScripts);
+                var editorSubFolder = String.Format("{0}Editor{0}", Path.DirectorySeparatorChar);
+                var pluginSubFolder = String.Format("{0}Plugins{0}", Path.DirectorySeparatorChar);
+
+                Console.WriteLine($"Editor: {editorSubFolder}\r\nPlugin: {pluginSubFolder}");
+
+                var allFiles = Directory.GetFiles(Path.Combine(options.Value.ProjectPath, "Assets"), "*.js", SearchOption.AllDirectories);
+                var filter = new Regex(string.Format(@"{0}{1}{0}|{0}{2}{0}", Path.DirectorySeparatorChar, editorSubFolder, pluginSubFolder), RegexOptions.Compiled);
+
+                var runtimeScripts = allFiles.Where(scriptPath => !filter.IsMatch(scriptPath)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath)});
+                var editorScripts = allFiles.Where(scriptPath => scriptPath.Contains(editorSubFolder)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath) });
+                var pluginScripts = allFiles.Where(scriptPath => scriptPath.Contains(pluginSubFolder)).Select(scriptPath => new SourceFile { FileName = scriptPath, Contents = File.ReadAllText(scriptPath) });
+
+                if (options.Value.DumpScripts)
+                {
+                    DumpScripts("Runtime", runtimeScripts);
+                    DumpScripts("Editor", editorScripts);
+                    DumpScripts("Plugin", pluginScripts);
+                }
+
+                var converter = new UnityScript2CSharpConverter(options.Value.IgnoreErrors);
+
+                var references = AssemblyReferencesFrom(options);
+
+                if (!ValidateAssemblyReferences(options))
+                    return -1;
+
+                ConvertScripts("runtime", runtimeScripts, converter, references, options.Value.Symbols, options.Value.RemoveOriginalFiles);
+                ConvertScripts("editor", editorScripts, converter, references, options.Value.Symbols, options.Value.RemoveOriginalFiles);
+                ConvertScripts("plugins", pluginScripts, converter, references, options.Value.Symbols, options.Value.RemoveOriginalFiles);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (options.Value.Verbose)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            return 0;
+        }
 
-            var converter = new UnityScript2CSharpConverter(options.Value.IgnoreErrors);
+        private static bool IsValid(ParserResult<CommandLineArguments> options)
+        {
+            var hasErrors = options.Errors.Any();
+            if (hasErrors)
+                return false;
 
+            // For some reason the command line parser is not detecting missing required arguments if we have multiple options
+            // marked as required.
+            if (string.IsNullOrWhiteSpace(options.Value.UnityPath) || string.IsNullOrWhiteSpace(options.Value.ProjectPath))
+            {
+                Console.WriteLine(HelpText.AutoBuild(options).ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private static List<string> AssemblyReferencesFrom(ParserResult<CommandLineArguments> options)
+        {
             var references = new List<string>(options.Value.References);
             references.Add(typeof(object).Assembly.Location);
 
-            if (!ValidateAssemblyReferences(options))
-                return -1;
+            string unityAssembliesRootPath;
+            if (!TryFindUnityAssembliesRoot(options.Value.UnityPath, options.Value.Verbose, out unityAssembliesRootPath))
+            {
+                var previous = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not find UnityEngine.dll / UnityEditor.dll in {0}", options.Value.UnityPath);
+                Console.ForegroundColor = previous;
+                return references;
+            }
 
-            ConvertScripts("runtime", runtimeScripts, converter, references, options.Value.Defines, options.Value.RemoveOriginalFiles);
-            ConvertScripts("editor", editorScripts, converter, references, options.Value.Defines, options.Value.RemoveOriginalFiles);
-            ConvertScripts("plugins", pluginScripts, converter, references, options.Value.Defines, options.Value.RemoveOriginalFiles);
+            references.Add(Path.Combine(unityAssembliesRootPath, "UnityEngine.dll"));
+            references.Add(Path.Combine(unityAssembliesRootPath, "UnityEditor.dll"));
 
-            return 0;
+            return references;
+        }
+
+        private static bool TryFindUnityAssembliesRoot(string testPath, bool verbose, out string unityAssembliesRootPath)
+        {
+            if (verbose)
+                Console.WriteLine("Probbing {0}", testPath);
+
+            var found = Directory.GetFiles(testPath, "*.dll").Any(file => file.Contains("UnityEngine"));
+            if (found)
+            {
+                unityAssembliesRootPath = testPath;
+                return true;
+            }
+
+            var folders = Directory.GetDirectories(testPath);
+            foreach (var folder in folders)
+            {
+                if (TryFindUnityAssembliesRoot(folder, verbose, out unityAssembliesRootPath))
+                    return true;
+            }
+
+            unityAssembliesRootPath = null;
+            return false;
         }
 
         private static bool ValidateAssemblyReferences(ParserResult<CommandLineArguments> options)
